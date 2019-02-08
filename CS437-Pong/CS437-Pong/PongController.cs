468f3012cdd4d540fbd1459eb7a8bb6ac891f683
Joel May <jmay@iastate.edu>
2019-02-08 13:14:40 -0600

    Player controls and bounds

--- CS437-Pong/CS437-Pong/PongController.cs
+++ CS437-Pong/CS437-Pong/PongController.cs
@@ -23,9 +23,45 @@ namespace CS437_Pong
         /// <param name="t">The amount of time that has passed.</param>
         public void updateModel(GameTime t)
         {
+            // ----- User Input -----
+            // Player 1
             if (Keyboard.GetState().IsKeyDown(KeyMap.W))
             {
+                model.player1Pos -= (float)t.ElapsedGameTime.TotalSeconds;
+            }
+            if (Keyboard.GetState().IsKeyDown(KeyMap.S))
+            {
                 model.player1Pos += (float)t.ElapsedGameTime.TotalSeconds;
+            }
+
+            // Player 2
+            // Note, this is not using KeyMap because it is assumed that the keyboard layout won't change arrow keys.
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                model.player2Pos -= (float)t.ElapsedGameTime.TotalSeconds;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                model.player2Pos += (float)t.ElapsedGameTime.TotalSeconds;
+            }
+
+            // ----- Keep everything in bounds -----
+            // Player 1
+            if (model.player1Pos < 0)
+            {
+                model.player1Pos = 0;
+            }
+            else if (model.player1Pos > 1)
+            {
+                model.player1Pos = 1;
+            }
+            // Player 2
+            if (model.player2Pos < 0)
+            {
+                model.player2Pos = 0;
+            }
+            else if (model.player2Pos > 1) {
+                model.player2Pos = 1;
             }
         }
     }
